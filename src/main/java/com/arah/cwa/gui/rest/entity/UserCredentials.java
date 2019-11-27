d63ec7dc2fb44ea68a5ea5b685f862eb10083c6e
nyurara <nyutamama@yandex.ru>
2019-11-27 19:56:32 +0300

    added forms

--- src/main/java/com/arah/cwa/gui/rest/entity/UserCredentials.java
+++ src/main/java/com/arah/cwa/gui/rest/entity/UserCredentials.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCredentials {
-
     private String username;
 
     private String password;
--- src/main/java/com/arah/cwa/gui/ui/main/AuthenticationFrame.java
+++ src/main/java/com/arah/cwa/gui/ui/main/AuthenticationFrame.java
@@ -1,9 +1,15 @@
 package com.arah.cwa.gui.ui.main;
 
+import com.arah.cwa.gui.rest.entity.User;
+import com.arah.cwa.gui.rest.entity.UserCredentials;
+import com.arah.cwa.gui.rest.producer.UserProducer;
+import com.arah.cwa.gui.security.SecurityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 @Component
 public class AuthenticationFrame extends JFrame {
@@ -14,10 +20,40 @@ public class AuthenticationFrame extends JFrame {
     private JLabel passwordLabel;
     private JPanel panel;
 
+    @Autowired
+    private SecurityManager securityManager;
+
+    @Autowired
+    private UserProducer userProducer;
+
     public AuthenticationFrame() {
         setContentPane(panel);
         setVisible(true);
-
+        initButtons();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
+
+    private void initButtons() {
+
+        //здесь типа есть в базе пользователь с кредами qwerty/qwerty
+        UserCredentials userCredentials = new UserCredentials();
+        userCredentials.setUsername("qwerty");
+        userCredentials.setPassword("qwerty");
+
+        OKButton.addActionListener(actionEvent -> {
+            securityManager.refreshToken(userCredentials);
+
+            List<User> all = userProducer.findAll();
+            User user = all.get(0);
+            JOptionPane.showMessageDialog(
+                    null,
+                    user.getLogin() + ", hello my friend!",
+                    "" + user.getEmail(),
+                    JOptionPane.INFORMATION_MESSAGE);
+        });
+        
+        getContentPane().add(panel);
+    }
+
+
 }
